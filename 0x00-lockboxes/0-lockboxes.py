8f972fb788795b023a055ce3ff21f9381c66f357
fdetun <fouedads@gmail.com>
2020-12-10 21:43:03 +0100

    sds

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
 """0-lockboxes"""
 
+
 def canUnlockAll(boxes):
     """ i can do all """
     if len(boxes) == 0:
         return False
-    if type(boxes) is not list:
+    if not isinstance(boxes, list):
         return False
     else:
         lenbox = len(boxes)
-    arr=[0]
+    arr = [0]
     j = 0
     for i in arr:
         try:
             for j in boxes[i]:
-                if type(j) is int:
+                if isinstance(j, int):
                     if j <= lenbox and j not in arr and j != 0:
                         arr.append(j)
-        except:
+        except BaseException:
             pass
     if len(arr) == lenbox:
         return True

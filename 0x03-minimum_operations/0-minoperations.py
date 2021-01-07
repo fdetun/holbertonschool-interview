82a8d0e55bbd21e7236cf63dcf65576cd0d3fc31
Foued Lamine <fouedads@gmail.com>
2021-01-07 23:40:06 +0100

    0-minoperations.py

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""minop"""
+
+
+def minOperations(n):
+    """ min ops"""
+    i = 2
+    if n < 2:
+        return 0
+    while i < n + 1:
+        i = i + 1
+        if n % i == 0:
+            return minOperations(n // i) + i

cfe991223d198db034791fdc7e39048da7963316
Foued Lamine <fouedads@gmail.com>
2021-04-08 00:17:37 +0100

    0-starwars_characters.js

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -2,15 +2,15 @@
 
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, { json: true }, (err, res, body) => {
+request(url, { json: true }, async (err, res, body) => {
   if (err) { return console.log(err); }
   const charurls = body.characters;
   for (const charindx in charurls) {
-    request(charurls[charindx], { json: true }, function (er, res, body) {
-      if (er) {
-        return console.log(er);
-      }
-      console.log(body.name);
+    const data = await new Promise((e, r) => {
+      request(charurls[charindx], { json: true }, function (er, res, body) {
+        if (er) { r(err); } else { e(body.name); }
+      });
     });
+    console.log(data);
   }
 });

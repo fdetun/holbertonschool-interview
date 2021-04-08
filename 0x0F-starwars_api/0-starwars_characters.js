4d43b5438df2ea8bc6f4ac1440dd73c00c5f68f8
Foued Lamine <fouedads@gmail.com>
2021-04-08 01:03:57 +0100

    0-starwars_characters.js

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -6,9 +6,9 @@ request(url, { json: true }, async (err, res, body) => {
   if (err) { return console.log(err); }
   const charurls = body.characters;
   for (const charindx in charurls) {
-    const data = await new Promise((e, r) => {
+    const data = await new Promise((resolve, reject) => {
       request(charurls[charindx], { json: true }, function (er, res, body) {
-        if (er) { r(err); } else { e(body.name); }
+        if (er) { reject(err); } else { resolve(body.name); }
       });
     });
     console.log(data);

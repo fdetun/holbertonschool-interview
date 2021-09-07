7760c750d70724d227d7e7ae1eece2a869beb5ab
Foued LAMINE <fouedads@gmail.com>
2021-09-07 19:38:43 +0100

    add node

---
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -0,0 +1,82 @@
+#include "list.h"
+
+/**
+ * add_node_end - add node to the end
+ * @list: list
+ * @str: node to add
+ * Return: list or NULL
+ */
+
+List *add_node_end(List **list, char *str)
+{
+List *new = NULL;
+
+	new = malloc(sizeof(List));
+	if (!new)
+		return (NULL);
+
+	new->str = strdup(str);
+	if (!new->str)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->next = NULL;
+	new->prev = NULL;
+
+if (!(*list))
+{
+new->next = new;
+new->prev = new;
+*list = new;
+}
+	else
+	{
+		new->next = (*list);
+		new->prev = (*list)->prev;
+		(*list)->prev->next = new;
+		(*list)->prev = new;
+	}
+	return (new);
+}
+
+/**
+ * add_node_begin - add node at the begin of the list
+ * @list: list
+ * @str: node to add
+ * Return: list or NULL
+ */
+
+List *add_node_begin(List **list, char *str)
+{
+List *new = NULL;
+
+	new = malloc(sizeof(List));
+	if (!new)
+		return (NULL);
+
+	new->str = strdup(str);
+	if (!new->str)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->next = NULL;
+	new->prev = NULL;
+
+if (!(*list))
+{
+newn->next = new;
+newn->prev = new;
+*list = new;
+}
+	else
+	{
+		new->next = (*list);
+		new->prev = (*list)->prev;
+		(*list)->prev->next = new;
+		(*list)->prev = new;
+*list = new;
+	}
+	return (new);
+}
